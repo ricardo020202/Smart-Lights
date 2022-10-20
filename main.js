4e362e7fbf2703bc06fc0ca7525a6e1cf83a7701
rico020202 <ricoroca2@gmail.com>
2022-10-19 21:26:30 -0500

    Correct and add UI elements

---
+++ main.js
@@ -0,0 +1,113 @@
+var count1 = 1, count2 = 1, count3 = 1, count4 = 1, count5 = 1, count6 = 1, count7 = 1, count8 = 1, count9 = 1, count10 = 1, count11 = 1;
+var button = document.querySelectorAll(".button");
+
+function changeColor1() {
+    if (count1 == 0) {
+        button[0].style.backgroundColor = "rgba(255, 255, 255, .70)";
+        count1 = 1;
+    } else {
+        button[0].style.backgroundColor = "rgba(255, 255, 0, .70)";
+        count1 = 0;
+    }
+}
+
+function changeColor2() {
+    if (count2 == 0) {
+        button[1].style.backgroundColor = "rgba(255, 255, 255, .70)";
+        count2 = 1;
+    } else {
+        button[1].style.backgroundColor = "rgba(255, 255, 0, .70)";
+        count2 = 0;
+    }
+}
+
+function changeColor3() {
+    if (count3 == 0) {
+        button[2].style.backgroundColor = "rgba(255, 255, 255, .70)";
+        count3 = 1;
+    } else {
+        button[2].style.backgroundColor = "rgba(255, 255, 0, .70)";
+        count3 = 0;
+    }
+}
+
+function changeColor4() {
+    if (count4 == 0) {
+        button[3].style.backgroundColor = "rgba(255, 255, 255, .70)";
+        count4 = 1;
+    } else {
+        button[3].style.backgroundColor = "rgba(255, 255, 0, .70)";
+        count4 = 0;
+    }
+}
+
+function changeColor5() {
+    if (count5 == 0) {
+        button[4].style.backgroundColor = "rgba(255, 255, 255, .70)";
+        count5 = 1;
+    } else {
+        button[4].style.backgroundColor = "rgba(255, 255, 0, .70)";
+        count5 = 0;
+    }
+}
+
+function changeColor6() {
+    if (count6 == 0) {
+        button[5].style.backgroundColor = "rgba(255, 255, 255, .70)";
+        count6 = 1;
+    } else {
+        button[5].style.backgroundColor = "rgba(255, 255, 0, .70)";
+        count6 = 0;
+    }
+}
+
+function changeColor7() {
+    if (count7 == 0) {
+        button[6].style.backgroundColor = "rgba(255, 255, 255, .70)";
+        count7 = 1;
+    } else {
+        button[6].style.backgroundColor = "rgba(255, 255, 0, .70)";
+        count7 = 0;
+    }
+}
+
+function changeColor8() {
+    if (count8 == 0) {
+        button[7].style.backgroundColor = "rgba(255, 255, 255, .70)";
+        count8 = 1;
+    } else {
+        button[7].style.backgroundColor = "rgba(255, 255, 0, .70)";
+        count8 = 0;
+    }
+}
+
+function changeColor9() {
+    if (count9 == 0) {
+        button[8].style.backgroundColor = "rgba(255, 255, 255, .70)";
+        count9 = 1;
+    } else {
+        button[8].style.backgroundColor = "rgba(255, 255, 0, .70)";
+        count9 = 0;
+    }
+}
+
+function changeColor10() {
+    if (count10 == 0) {
+        button[9].style.backgroundColor = "rgba(255, 255, 255, .70)";
+        count10 = 1;
+    } else {
+        button[9].style.backgroundColor = "rgba(255, 255, 0, .70)";
+        count10 = 0;
+    }
+}
+
+function changeColor11() {
+    if (count11 == 0) {
+        button[10].style.backgroundColor = "rgba(255, 255, 255, .70)";
+        count11 = 1;
+    } else {
+        button[10].style.backgroundColor = "rgba(255, 255, 0, .70)";
+        count11 = 0;
+    }
+}
+
