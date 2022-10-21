73479becfe079086cee75fbd32e8ae3cc3d61245
ricardo020202 <ricoroca2@hotmail.com>
2022-10-20 21:28:30 -0500

    Add final version

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
-var count1 = 1, count2 = 1, count3 = 1, count4 = 1, count5 = 1, count6 = 1, count7 = 1, count8 = 1, count9 = 1, count10 = 1, count11 = 1;
+var count1 = 1, count2 = 1, count3 = 1, count4 = 1, count5 = 1, 
+count6 = 1, count7 = 1, count8 = 1, count9 = 1, count10 = 1, count11 = 1,
+count12 = 1, count13 = 1, count14 = 1, count15 = 1, count16 = 1, 
+count17 = 1, count18 = 1, count19 = 1, count20 = 1, count21 = 1, count22 = 1;
 var button = document.querySelectorAll(".button");
 
 function changeColor1() {
@@ -111,3 +114,199 @@ function changeColor11() {
     }
 }
 
+function changeColor12() {
+    if (count12 == 0) {
+        button[11].style.backgroundColor = "rgba(255, 255, 255, .70)";
+        count12 = 1;
+    } else {
+        button[11].style.backgroundColor = "rgba(255, 255, 0, .70)";
+        count12 = 0;
+    }
+}
+
+function changeColor13() {
+    if (count13 == 0) {
+        button[12].style.backgroundColor = "rgba(255, 255, 255, .70)";
+        count13 = 1;
+    } else {
+        button[12].style.backgroundColor = "rgba(255, 255, 0, .70)";
+        count13 = 0;
+    }
+}
+
+function changeColor14() {
+    if (count14 == 0) {
+        button[13].style.backgroundColor = "rgba(255, 255, 255, .70)";
+        count14 = 1;
+    } else {
+        button[13].style.backgroundColor = "rgba(255, 255, 0, .70)";
+        count14 = 0;
+    }
+}
+
+function changeColor15() {
+    if (count15 == 0) {
+        button[14].style.backgroundColor = "rgba(255, 255, 255, .70)";
+        count15 = 1;
+    } else {
+        button[14].style.backgroundColor = "rgba(255, 255, 0, .70)";
+        count15 = 0;
+    }
+}
+
+function changeColor16() {
+    if (count16 == 0) {
+        button[15].style.backgroundColor = "rgba(255, 255, 255, .70)";
+        count16 = 1;
+    } else {
+        button[15].style.backgroundColor = "rgba(255, 255, 0, .70)";
+        count16 = 0;
+    }
+}
+
+function changeColor17() {
+    if (count17 == 0) {
+        button[16].style.backgroundColor = "rgba(255, 255, 255, .70)";
+        count17 = 1;
+    } else {
+        button[16].style.backgroundColor = "rgba(255, 255, 0, .70)";
+        count17 = 0;
+    }
+}
+
+function changeColor18() {
+    if (count18 == 0) {
+        button[17].style.backgroundColor = "rgba(255, 255, 255, .70)";
+        count18 = 1;
+    } else {
+        button[17].style.backgroundColor = "rgba(255, 255, 0, .70)";
+        count18 = 0;
+    }
+}
+
+function changeColor19() {
+    if (count19 == 0) {
+        button[18].style.backgroundColor = "rgba(255, 255, 255, .70)";
+        count19 = 1;
+    } else {
+        button[18].style.backgroundColor = "rgba(255, 255, 0, .70)";
+        count19 = 0;
+    }
+}
+
+function changeColor20() {
+    if (count20 == 0) {
+        button[19].style.backgroundColor = "rgba(255, 255, 255, .70)";
+        count20 = 1;
+    } else {
+        button[19].style.backgroundColor = "rgba(255, 255, 0, .70)";
+        count20 = 0;
+    }
+}
+
+function changeColor21() {
+    if (count21 == 0) {
+        button[20].style.backgroundColor = "rgba(255, 255, 255, .70)";
+        count21 = 1;
+    } else {
+        button[20].style.backgroundColor = "rgba(255, 255, 0, .70)";
+        count21 = 0;
+    }
+}
+
+function changeColor22() {
+    if (count22 == 0) {
+        button[21].style.backgroundColor = "rgba(255, 255, 255, .70)";
+        count22 = 1;
+    } else {
+        button[21].style.backgroundColor = "rgba(255, 255, 0, .70)";
+        count22 = 0;
+    }
+}
+
+function timePicker(id){
+    var input = document.getElementById(id);
+    var timePicker = document.createElement('div');
+    timePicker.classList.add('time-picker');
+    input.value = '08:30';
+   
+    //open timepicker
+    input.onclick= function(){
+       timePicker.classList.toggle('open');
+       
+       this.setAttribute('disabled','disabled');
+       timePicker.innerHTML +=`
+       <div class="set-time">
+          <div class="label">
+             <a id="plusH" >+</a>
+             <input class="set" type="text" id="hour" value="08">
+             <a id="minusH">-</a>
+          </div>
+          <div class="label">
+             <a id="plusM">+</a>
+             <input class="set" type="text" id="minute" value="30">
+             <a id="minusM">-</a>
+          </div>
+       </div>
+       <div id="submitTime">Set time</div>`;
+       this.after(timePicker);
+       var plusH = document.getElementById('plusH');
+       var minusH = document.getElementById('minusH');
+       var plusM = document.getElementById('plusM');
+       var minusM = document.getElementById('minusM');
+       var h = parseInt(document.getElementById('hour').value);
+       var m = parseInt(document.getElementById('minute').value);
+      //increment hour
+       plusH.onclick = function(){
+          h = isNaN(h) ? 0 : h;
+          if(h===23){
+             h =-1;
+          }
+           h++; 
+          document.getElementById('hour').value = (h<10?'0':0)+h;
+       }
+       //decrement hour
+       minusH.onclick = function(){
+          h = isNaN(h) ? 0 : h;
+          if(h===0){
+             h =24;
+          }
+          h--;
+          document.getElementById('hour').value = (h<10?'0':0)+h;
+       }
+       //increment hour
+       plusM.onclick = function(){
+          m = isNaN(m) ? 0 : m;
+          if(m===45){
+             m =-15; 
+          }
+           m = m+15; 
+          document.getElementById('minute').value = (m<10?'0':0)+m;
+       }
+       //decrement hour
+       minusM.onclick = function(){
+         m = isNaN(m) ? 0 : m;
+          if(m===0){
+             m =60;
+          }
+          m = m-15;
+          document.getElementById('minute').value = (m<10?'0':0)+m;
+       }
+       
+       //submit timepicker
+       var submit = document.getElementById("submitTime");
+       submit.onclick = function(){
+         input.value = document.getElementById('hour').value+':'+document.getElementById('minute').value;
+          input.removeAttribute('disabled');
+          timePicker.classList.toggle('open');
+          timePicker.innerHTML = '';
+       }
+    }
+ }
+ 
+timePicker('timePicker');
+timePicker('timePicker2');
+timePicker('timePicker3');
+timePicker('timePicker4');
+ 
+ 
